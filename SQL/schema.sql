b0bfef354dae99a9abadf4fce75ccd10db64a2b7
Hack Reactor Students <ajay.tungare@gmail.com>
2014-06-27 16:36:38 -0700

    move to another temial b/c workstation not working

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,14 +1,29 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  room_id int NOT NULL,
+  room_name varchar(20),
+  PRIMARY KEY (room_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE users (
+  user_id int NOT NULL,
+  user_name varchar(20),
+  PRIMARY KEY (user_id)
+) ENGINE = InnoDB;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
-/* Create other tables and define schemas for them here! */
-
-
+  msg_id int NOT NULL,
+  msg_text varchar(140),
+  room_id int NOT NULL,
+  user_id int NOT NULL,
+  created_at TIMESTAMP,
+  FOREIGN KEY (room_id) REFERENCES rooms(room_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  PRIMARY KEY (msg_id)
+) ENGINE = InnoDB;
 
 
 /*  Execute this file from the command line by typing:
